01d059fa132f435d19d2aadc8a2fae6df92aa8be
Erik Tesar <erik@erik-tesar.com>
2021-07-12 19:46:10 +0200

    endpoint const

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,19 @@ use async_graphql::{EmptyMutation, EmptySubscription, Schema};
 use async_graphql_actix_web::{Request, Response};
 use starwars::{QueryRoot, StarWars, StarWarsSchema};
 
+const GRAPHQL_ENDPOINT: &str = "/graphql";
+const GRAPHQL_PLAYGROUND_ENDPOINT: &str = "/playground";
 
-async fn index(schema: web::Data<StarWarsSchema>, req: Request) -> Response {
+async fn graphql(schema: web::Data<StarWarsSchema>, req: Request) -> Response {
     schema.execute(req.into_inner()).await.into()
 }
 
-async fn index_playground() -> Result<HttpResponse> {
+async fn playground() -> Result<HttpResponse> {
     Ok(HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
         .body(playground_source(
-            GraphQLPlaygroundConfig::new("/").subscription_endpoint("/"),
-        )))
+            GraphQLPlaygroundConfig::new(GRAPHQL_ENDPOINT)),
+        ))
 }
 
 #[actix_web::main]
@@ -26,14 +28,14 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         let mut app = App::new()
             .data(schema.clone())
-            .service(web::resource("/").guard(guard::Post()).to(index));
+            .service(web::resource(GRAPHQL_ENDPOINT).guard(guard::Post()).to(graphql));
         println!("Playground: http://localhost:8000");
         if cfg!(debug_assertions) {
-            app = app.service(web::resource("/").guard(guard::Get()).to(index_playground));
+            app = app.service(web::resource(GRAPHQL_PLAYGROUND_ENDPOINT).guard(guard::Get()).to(playground));
         }
         app
     })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
