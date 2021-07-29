89e694a35935b2e151de241c908b58a2adfeeaf0
Erik Tesar <erik@erik-tesar.com>
2021-07-29 18:18:01 +0200

    add basic schema for testing

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 use actix_web::{guard, web, App, HttpResponse, HttpServer, Result};
 use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
-use async_graphql::{EmptyMutation, EmptySubscription, Schema};
-use async_graphql_actix_web::{Request, Response};
+use async_graphql::{EmptySubscription, Schema};
+use async_graphql_actix_web::{Request};
 use sqlx::{PgPool, migrate};
 use std::fs::read_to_string;
-use crate::models::graphql::schema::{Azuma, AzumaSchema, QueryRoot};
+use crate::models::graphql::schema::{AzumaSchema, QueryRoot, MutationRoot};
 use serde::Deserialize;
 use actix_web::body::Body;
 use actix_web::web::Bytes;
+use actix_web::{post};
 
 const GRAPHQL_ENDPOINT: &str = "/graphql";
 const GRAPHQL_PLAYGROUND_ENDPOINT: &str = "/playground";
 
 mod models;
 
+#[post("/graphql")]
 async fn graphql(schema: web::Data<AzumaSchema>, req: Request) -> HttpResponse {
     HttpResponse::Ok().body(Body::Bytes(Bytes::from(serde_cbor::to_vec(&schema.execute(req.into_inner()).await).unwrap())))
 }
@@ -54,14 +56,13 @@ async fn main() -> std::io::Result<()> {
         .await
         .expect("couldn't run database migrations");
 
-    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
-        .data(Azuma::new(db))
+    let schema = Schema::build(QueryRoot, MutationRoot, EmptySubscription)
         .finish();
 
     HttpServer::new(move || {
         let mut app = App::new()
             .data(schema.clone())
-            .service(web::resource(GRAPHQL_ENDPOINT).guard(guard::Post()).to(graphql))
+            .service(graphql)
             .route("/graphql/sdl", web::get().to(getsdl));
         //println!("Playground: http://{}", config.host_uri);
         if cfg!(debug_assertions) {
--- src/models/graphql/schema.rs
+++ src/models/graphql/schema.rs
@@ -1,29 +1,46 @@
-use async_graphql::{Schema, EmptyMutation, EmptySubscription, Context, ID, Object};
-use sqlx::PgPool;
+use async_graphql::{Schema, EmptySubscription, ID, Object, Result, Error};
 use uuid::Uuid;
 
-pub type AzumaSchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
+pub type AzumaSchema = Schema<QueryRoot, MutationRoot, EmptySubscription>;
 
 pub struct QueryRoot;
+pub struct MutationRoot;
 
-pub struct Azuma {
-    db: PgPool,
-}
 
-impl Azuma {
-    pub fn new(db: PgPool) -> Self {
-        Self {
-            db
+#[Object]
+impl QueryRoot {
+    async fn user(&self, username: String) -> User {
+        User {
+            id: ID(Uuid::new_v4().to_string()),
+            username: username.to_string(),
+            cert: vec![0x42u8]
         }
     }
 }
 
 #[Object]
-impl QueryRoot {
-    async fn getSessionToken(&self,
-                             ctx: &Context<'_>,
-                             id: ID,
-    ) -> ID {
-        ID(Uuid::new_v4().to_string())
+impl MutationRoot {
+    async fn signup(&self, _username: String) -> Result<User> {
+        Err(Error { message: "Failed".to_string(), extensions: None})
+    }
+}
+struct User {
+    id: ID,
+    username: String,
+    cert: Vec<u8>
+}
+
+#[Object]
+impl User {
+    async fn id(&self) -> ID {
+        self.id.clone()
+    }
+
+    async fn name(&self) -> String {
+        self.username.clone()
+    }
+
+    async fn cert(&self) -> Vec<u8> {
+        self.cert.clone()
     }
 }
