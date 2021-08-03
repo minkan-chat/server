80a89798a73c20f7dd67aa540821ff3d5a6c863a
Erik Tesar <erik@erik-tesar.com>
2021-08-03 13:20:24 +0200

    fix: add all graphql variants to Error interface

--- src/models/graphql/interfaces.rs
+++ src/models/graphql/interfaces.rs
@@ -2,16 +2,32 @@ use async_graphql::{Interface, ID};
 
 use super::{
     scalars::Bytes,
-    types::{PrivateCertificate, PublicCertificate, Server, User, UsernameUnavailable},
+    types::{
+        CertificateTaken, InvalidCetificate, InvalidMasterPasswordHash, InvalidSignature,
+        PrivateCertificate, PublicCertificate, Server, UnknownUser, User, UserSuspended,
+        UsernameUnavailable,
+    },
 };
 
 #[derive(Interface)]
 #[graphql(
-    field(name = "id", type = "ID", desc = "The unique identify (UUID) of the Actor as defined in RFC 4122"),
-    field(name = "certificate", type = "PublicCertificate", desc = "The certificate of the Actor without secret parts."),
-    field(name = "name", type = "String", desc = "The name of the Actor as an utf-8 string"),
+    field(
+        name = "id",
+        type = "ID",
+        desc = "The unique identify (UUID) of the Actor as defined in RFC 4122"
+    ),
+    field(
+        name = "certificate",
+        type = "PublicCertificate",
+        desc = "The certificate of the Actor without secret parts."
+    ),
+    field(
+        name = "name",
+        type = "String",
+        desc = "The name of the Actor as an utf-8 string"
+    )
 )]
-/// Represents an Actor. Could be a User or a Server 
+/// Represents an Actor. Could be a User or a Server
 pub(crate) enum Actor {
     Server(Server),
     User(User),
@@ -19,8 +35,16 @@ pub(crate) enum Actor {
 
 #[derive(Interface)]
 #[graphql(
-    field(name = "fingerprint", type = "String", desc = "The long-form fingerprint of the Certificate as a hex string"),
-    field(name = "content", type = "Bytes", desc = "The whole certificate as bytes")
+    field(
+        name = "fingerprint",
+        type = "String",
+        desc = "The long-form fingerprint of the Certificate as a hex string"
+    ),
+    field(
+        name = "content",
+        type = "Bytes",
+        desc = "The whole certificate as bytes"
+    )
 )]
 /// Represents a PGP Certificate
 pub(crate) enum Certificate {
@@ -32,5 +56,11 @@ pub(crate) enum Certificate {
 #[graphql(field(name = "message", type = "String"))]
 /// Generic error type used for typed errors
 pub(crate) enum Error {
-    UsernameTaken(UsernameUnavailable),
+    UsernameUnavailable(UsernameUnavailable),
+    CertificateTaken(CertificateTaken),
+    InvalidCetificate(InvalidCetificate),
+    InvalidSignature(InvalidSignature),
+    UnknownUser(UnknownUser),
+    InvalidMasterPasswordHash(InvalidMasterPasswordHash),
+    UserSuspended(UserSuspended),
 }
