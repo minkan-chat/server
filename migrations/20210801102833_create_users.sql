6c8bf096494c8277fc2b5dd2713c5f53a39d0565
Erik Tesar <erik@erik-tesar.com>
2021-08-07 23:13:23 +0200

    fix: database: make username required (NOT NULL) and UNIQUE

--- migrations/20210801102833_create_users.sql
+++ migrations/20210801102833_create_users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     enc_cert bytea UNIQUE,
-    username text NOT NULL,
+    username text UNIQUE NOT NULL,
     master_pw_hash text NOT NULL,
     created_at timestamp with time zone NOT NULL DEFAULT current_timestamp,
     suspended bool NOT NULL DEFAULT false,
