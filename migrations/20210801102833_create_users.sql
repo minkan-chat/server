2ae662c1ce70e34e37f8f62c53498c6fb966631e
Erik Tesar <erik@erik-tesar.com>
2021-08-07 20:36:50 +0200

    chore: let master_pw_hash be a string instead of bytea

--- migrations/20210801102833_create_users.sql
+++ migrations/20210801102833_create_users.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     enc_cert bytea UNIQUE,
     username text NOT NULL,
-    master_pw_hash bytea NOT NULL,
+    master_pw_hash text NOT NULL,
     created_at timestamp with time zone NOT NULL DEFAULT current_timestamp,
     suspended bool NOT NULL DEFAULT false,
     suspended_reason TEXT DEFAULT NULL
