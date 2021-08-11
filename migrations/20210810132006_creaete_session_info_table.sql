cd3e5c3a5c0b9ea716e91ffc5a248109c68f4a65
Erik Tesar <erik@erik-tesar.com>
2021-08-11 15:11:06 +0200

    refactor: session info: don't store unnecessary metadata (remove ip_address and user_agent)

--- migrations/20210810132006_creaete_session_info_table.sql
+++ migrations/20210810132006_creaete_session_info_table.sql
@@ -2,12 +2,6 @@ CREATE TABLE session_info (
     session_id UUID NOT NULL UNIQUE DEFAULT gen_random_uuid(),
     -- Not unique because a user can have multiple sessions. We need an index here, create one!
     user_id UUID NOT NULL REFERENCES users(user_id),
-    -- the ip address of the client who logged in/belogs that session to
-    ip_address INET NOT NULL,
-    -- for browser or http clients, there's often a user agent e.g:
-    -- Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0
-    -- these can help a user to identify active sessions
-    user_agent TEXT NOT NULL,
     -- optionally a user can set a name to directly identify a session
     -- e.g. "Minkan on my Laptop"
     session_name VARCHAR(32)
