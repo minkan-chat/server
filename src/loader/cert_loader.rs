7f07c1a511baac14a1af153e7639d45ea27fb3b3
Erik Tesar <erik@erik-tesar.com>
2021-10-17 18:55:07 +0200

    chore: remove old code

--- src/loader/cert_loader.rs
+++ src/loader/cert_loader.rs
@@ -7,61 +7,6 @@ use futures::TryStreamExt;
 use sequoia_openpgp::Fingerprint;
 use uuid::Uuid;
 
-/*loader_struct!(PrivateCertificateLoader);
-
-use futures::stream::TryStreamExt;
-
-#[async_trait]
-impl Loader<uuid::Uuid> for PrivateCertificateLoader {
-    type Value = PrivateCertificate;
-    type Error = Arc<sqlx::Error>;
-    async fn load(
-        &self,
-        keys: &[uuid::Uuid],
-    ) -> Result<HashMap<uuid::Uuid, Self::Value>, Self::Error> {
-        Ok(sqlx::query!(
-            r#"
-                SELECT user_id, enc_cert FROM users WHERE user_id = ANY($1)
-                "#,
-            keys,
-        )
-        .fetch(&self.pool)
-        .map_ok(|record| {
-            let p =
-                PrivateCertificate::try_from(record.enc_cert).expect("invalid cert in database");
-            (record.user_id, p)
-        })
-        .try_collect()
-        .await?)
-    }
-}
-
-loader_struct!(PublicCertificateLoader);
-
-#[async_trait]
-impl Loader<uuid::Uuid> for PublicCertificateLoader {
-    type Value = PublicCertificate;
-    type Error = Arc<sqlx::Error>;
-    async fn load(
-        &self,
-        keys: &[uuid::Uuid],
-    ) -> Result<HashMap<uuid::Uuid, Self::Value>, Self::Error> {
-        Ok(sqlx::query!(
-            r#"
-                SELECT user_id, pub_cert FROM pub_certs WHERE user_id = ANY($1)
-                "#,
-            keys,
-        )
-        .fetch(&self.pool)
-        .map_ok(|record| {
-            let p = PublicCertificate::try_from(record.pub_cert).expect("invalid cert in database");
-            (record.user_id, p)
-        })
-        .try_collect()
-        .await?)
-    }
-}*/
-
 loader_struct!(PublicCertificateLoader);
 
 #[async_trait]
